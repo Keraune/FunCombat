bc90574e9742d8174a49b573b210cd21f89803a4
Keraune <framches985083@gmail.com>
2025-11-28 03:48:54 -0500

    first commit

---
+++ src/main/java/fc/keraune/funCombat/FunCombat.java
@@ -0,0 +1,17 @@
+package fc.keraune.funCombat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FunCombat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
